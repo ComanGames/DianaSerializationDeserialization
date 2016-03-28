087f8140d7098c58cc663b6339992004df16bc9d
Diana Dalkevych <diana.dalkevych@outlook.com>
2016-03-28 16:54:02 +0300

    changes in TXTSerializer

--- DianaSerializationDeserialization/TextLessonReaderWriter.cs
+++ DianaSerializationDeserialization/TextLessonReaderWriter.cs
@@ -1,15 +1,22 @@
-﻿namespace DianaSerializationDeserialization
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DianaSerializationDeserialization
 {
     internal class TextLessonReaderWriter : LessonReaderWriter
     {
         public override void SaveLesson(string path, LessonInfo lessonInfo)
         {
-            throw new System.NotImplementedException();
+            File.WriteAllText(path, lessonInfo.ToString());
         }
 
         public override LessonInfo LoadLesson(string path)
         {
-            throw new System.NotImplementedException();
+            LessonInfo lessonInfo = new LessonInfo();
+            File.ReadAllText(path);
+            return lessonInfo;
         }
     }
 }
