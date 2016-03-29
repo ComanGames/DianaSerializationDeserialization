9b9b84cab4f0d022d3f67e501daff25b1db1205c
Diana Dalkevych <diana.dalkevych@outlook.com>
2016-03-29 13:00:42 +0300

    some code

--- DianaSerializationDeserialization/Program.cs
+++ DianaSerializationDeserialization/Program.cs
@@ -9,6 +9,17 @@ namespace DianaSerializationDeserialization
         static void Main()
         {
             //Creating our lesson Info
+            Console.WriteLine("Please write down topic of current lesson");
+            string topic = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please put in lesson question");
+            string question1 = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("If you would like to add more questions, press Y");
+            if (Console.ReadKey().Key = ConsoleKey.Y)
+            {
+                
+            }
             string[] questions = new[] {"What Serialization", "Why we use it", "Where you will use it"};
             LessonInfo lessonInfo = new LessonInfo(120,"Serialization",questions);
             string lessonPath = @"C:\Temp\lesson";
