c7be73ec79cee7abb6b9b79a323eb6995d1b2a1c
ComanGames <alfaa.gen@gmail.com>
2016-03-29 12:59:14 +0300

    Oh fuck I forget to add binary ReaderWriter.cs

---
+++ SerializationLibery/BinaryLessonReaderWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializationLibery
+{
+    public class BinaryLessonReaderWriter : LessonReaderWriter
+    { 
+        public override void SaveLesson(string path, LessonInfo lessonInfo)
+        {
+            BinaryFormatter myBinFormatter = new BinaryFormatter();
+            using (Stream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                myBinFormatter.Serialize(fileStream, lessonInfo);
+            }
+        }
+
+        public override LessonInfo LoadLesson(string path)
+        {
+            LessonInfo lessonInfo;
+            BinaryFormatter mBinaryFormatter = new BinaryFormatter();
+            using (Stream fileStream = File.OpenRead(path))
+            {
+                lessonInfo = (LessonInfo) mBinaryFormatter.Deserialize(fileStream);
+            }
+            return lessonInfo;
+        }
+    }
+}
